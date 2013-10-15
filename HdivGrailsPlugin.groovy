749e6a7b9611567a510311a926455cb629bee3b3
Burt Beckwith <burt@burtbeckwith.com>
2013-10-15 19:34:45 -0400

    cleanup

--- HdivGrailsPlugin.groovy
+++ HdivGrailsPlugin.groovy
@@ -18,18 +18,18 @@ class HdivGrailsPlugin {
 	String grailsVersion = '2.3.1 > *'
 	String title = 'HDIV Plugin'
 	String author = 'Burt Beckwith'
-	String authorEmail = 'beckwithb@vmware.com'
+	String authorEmail = 'burt@burtbeckwith.com'
 	String description = 'HDIV plugin'
-	String documentation = 'http://grails.org/plugin/hdiv'
-	List loadAfter = ['controllers']
+	String documentation = 'http://burtbeckwith.github.io/grails-hdiv'
+	List loadAfter = ['controllers', 'springSecurityCore']
 	List pluginExcludes = [
 		'docs/**',
 		'src/docs/**'
 	]
 
 	String license = 'APACHE'
-//	def issueManagement = [system: 'JIRA', url: 'http://jira.grails.org/browse/GPMYPLUGIN']
 	def scm = [url: 'https://github.com/burtbeckwith/grails-hdiv']
+	def issueManagement = [system: 'Github', url: 'https://github.com/burtbeckwith/grails-hdiv/issues']
 
 	def getWebXmlFilterOrder() {
 		[ValidatorFilter: FilterManager.GRAILS_WEB_REQUEST_POSITION - 100]
@@ -77,31 +77,6 @@ class HdivGrailsPlugin {
 				maxUploadSize = 100000
 			}
 		}
-
-		/*
-		 <?xml version="1.0" encoding="UTF-8"?>
-		 <beans xmlns="http://www.springframework.org/schema/beans"
-			 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:hdiv="http://www.hdiv.org/schema/hdiv"
-			 xsi:schemaLocation="http://www.springframework.org/schema/beans http://www.springframework.org/schema/beans/spring-beans.xsd
-		 http://www.hdiv.org/schema/hdiv http://www.hdiv.org/schema/hdiv/hdiv.xsd">
-
-			 <hdiv:config errorPage="/error.jsp" excludedExtensions="css,png,ico">
-				 <hdiv:startPages>/,/attacks/.*,/login/auth,/j_spring_security_check,/grails-errorhandler</hdiv:startPages>
-			 </hdiv:config>
-
-			 <!-- Accepted pattern within the application for all editable parameters (generated from textbox and textarea) -->
-			 <hdiv:validation id="safeText">
-				 <hdiv:acceptedPattern><![CDATA[^[a-zA-Z0-9@.\-_]*$]]></hdiv:acceptedPattern>
-			 </hdiv:validation>
-
-			 <!-- Finally, it's necessary to define editable data validation list for the application -->
-			 <hdiv:editableValidations>
-				 <hdiv:validationRule url="/secure/.*">safeText</hdiv:validationRule>
-				 <hdiv:validationRule url="/safetext/.*" enableDefaults="false">safeText</hdiv:validationRule>
-			 </hdiv:editableValidations>
-
-		 </beans>
-		 */
 
 		def hdivConfig = HdivUtils.getConfig(application)
 

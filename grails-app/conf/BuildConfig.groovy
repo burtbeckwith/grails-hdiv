70b05f78d00b1b4b69034bbf00092a567db29e9f
Burt Beckwith <burt@burtbeckwith.com>
2013-10-15 13:26:59 -0400

    update to HDIV 2.1.5 and added webxml plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -14,21 +14,21 @@ grails.project.dependency.resolution = {
 
 	dependencies {
 
-		String hdivVersion = '2.1.4'
+		String hdivVersion = '2.1.5'
 
 		compile "org.hdiv:hdiv-core:$hdivVersion", {
 			excludes 'commons-codec', 'commons-fileupload', 'commons-logging', 'junit',
-			         'servlet-api', 'spring-test', 'spring-web'
+			         'servlet-api', 'spring-security-core', 'spring-test', 'spring-web'
 		}
 
 		compile "org.hdiv:hdiv-spring-mvc:$hdivVersion", {
-			excludes 'commons-logging', 'hdiv-core', 'jsp-api', 'junit',
-			         'servlet-api', 'spring-test', 'spring-webmvc'
+			excludes 'commons-logging', 'grails-core', 'hdiv-core', 'jsp-api', 'junit',
+			         'servlet-api', 'spring-test', 'spring-webmvc', 'validation-api'
 		}
 
 		compile "org.hdiv:hdiv-config:$hdivVersion", {
-			excludes 'commons-logging', 'hdiv-core', 'hdiv-spring-mvc', 'junit',
-			         'spring-test', 'spring-web'
+			excludes 'commons-logging', 'hdiv-core', 'hdiv-jsf', 'hdiv-spring-mvc', 'hibernate-validator',
+			         'junit', 'spring-test', 'spring-webmvc', 'validation-api'
 		}
 
 		runtime 'javax.servlet.jsp:jsp-api:2.1'
@@ -36,13 +36,15 @@ grails.project.dependency.resolution = {
 			excludes 'jsp-api'
 		}
 		compile "org.hdiv:hdiv-jstl-taglibs-1.2:$hdivVersion", {
-			excludes 'commons-logging', 'hdiv-core', 'jsp-api', 'jstl', 'junit', 'servlet-api'
+			excludes 'commons-logging', 'hdiv-core', 'jsp-api', 'jstl-api', 'jstl-impl', 'junit', 'servlet-api'
 		}
 
 		compile 'taglibs:standard:1.1.2'
 	}
 
 	plugins {
+		compile ':webxml:1.4.1'
+
 		build ':release:2.2.1', ':rest-client-builder:1.0.3', {
 			export = false
 		}

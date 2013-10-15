a0cad1a423ff50cb89a7dfbb0a8f6fae6fad2e46
Burt Beckwith <burt@burtbeckwith.com>
2013-10-15 19:21:17 -0400

    update to use Grails 2.3.1 for dev

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -45,7 +45,7 @@ grails.project.dependency.resolution = {
 	plugins {
 		compile ':webxml:1.4.1'
 
-		build ':release:2.2.1', ':rest-client-builder:1.0.3', {
+		build ':release:3.0.1', ':rest-client-builder:1.0.3', {
 			export = false
 		}
 	}

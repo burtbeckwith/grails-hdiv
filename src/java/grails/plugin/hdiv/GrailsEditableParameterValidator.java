e034bea01c4899f3467a74fccfd8aedc7d740e47
Burt Beckwith <burt@burtbeckwith.com>
2013-10-16 15:59:07 -0400

    update for listener changes in core

--- src/java/grails/plugin/hdiv/GrailsEditableParameterValidator.java
+++ src/java/grails/plugin/hdiv/GrailsEditableParameterValidator.java
@@ -1,23 +1,23 @@
 package grails.plugin.hdiv;
 
-import org.codehaus.groovy.grails.web.binding.DataBindingListener2;
 import org.grails.databinding.errors.BindingError;
+import org.grails.databinding.events.DataBindingListener;
 import org.hdiv.web.validator.EditableParameterValidator;
-import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 
 /**
- * Wrapper to implement the DataBindingListener2 interface.
+ * Wrapper to implement the DataBindingListener interface.
  *
  * @author <a href='mailto:burt@burtbeckwith.com'>Burt Beckwith</a>
  */
-public class GrailsEditableParameterValidator extends EditableParameterValidator implements DataBindingListener2 {
+public class GrailsEditableParameterValidator extends EditableParameterValidator implements DataBindingListener {
 
-	public void afterBinding(Object target, BindingResult errors) {
-		validate(target, errors);
+	public void afterBinding(Object target, Object errors) {
+		validate(target, (Errors) errors);
 	}
 
-	public Boolean beforeBinding(Object target, BindingResult errors) { return true; }
-	public Boolean beforeBinding(Object target, String propertyName, Object value) { return true; }
-	public void afterBinding(Object target, String propertyName) { /* no-op */ }
-	public void bindingError(BindingError error) { /* no-op */ }
+	public Boolean beforeBinding(Object target, Object errors) { return true; }
+	public Boolean beforeBinding(Object target, String propertyName, Object value, Object errors) { return true; }
+	public void afterBinding(Object target, String propertyName, Object errors) { /* no-op */ }
+	public void bindingError(BindingError error, Object errors) { /* no-op */ }
 }
